1d0607f8ddf722be94381d3433774c1a666a637d
Донир Доржиев <dsdorzhiev@edu.hse.ru>
2021-09-07 15:58:20 +0300

    start

---
+++ Task A/Task A.cpp
@@ -0,0 +1,29 @@
+﻿/* Программа вычисления среднего арифметического ряда чисел до нуля, не включая его */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Вводимое число
+	int inputNumber;
+	//сумма чисел, изначально равна 0; количество чисел, изначально равно 0
+	double sumOfNumbers = 0, numberCount = 0;
+
+	cin >> inputNumber;
+	while (inputNumber != 0)
+	{
+		//Складываем все числа
+		sumOfNumbers += inputNumber;
+		//Вводим новое число
+		cin >> inputNumber;
+		//Увеличиваем количество чисел
+		numberCount++;
+	}
+	
+	//Вывод с точностью 5 знаков после запятой
+	cout.precision(5);
+	cout << sumOfNumbers / numberCount;
+}
+
+
