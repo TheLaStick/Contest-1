b79bef82107ad933a07865dd4afa2eb3151bc45f
Донир Доржиев <dsdorzhiev@edu.hse.ru>
2021-09-12 13:02:09 +0300

    5 задач из 6

---
+++ Task B/Task B.cpp
@@ -0,0 +1,28 @@
+﻿/* Программа быстрого возведения числа в степень */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	//Число, которое нужно возвести в степень
+	double inputNumber;
+	//Степень, в которое нужно возвести число
+	int pow;
+	//Итоговый результат
+	double result = 1;
+
+	cin >> inputNumber;
+	cin >> pow;
+
+	while (pow > 0)
+	{
+		if (pow % 2 != 0) 
+		{
+			result *= inputNumber;
+		}
+		inputNumber *= inputNumber;
+		pow /= 2;
+	}
+	cout.precision(6);
+	cout << result;
+}
---
+++ Task C/Task C.cpp
@@ -0,0 +1,40 @@
+﻿/* Программа поиска количества натуральных палиндромов до числа K*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	//Вводимое число К
+    double K;
+	//перебираемое число number; 
+	//его копия для зеркаливания naturalNum; 
+	//цифра перебираемого числа digit; 
+	//отзеркаленное число mirror; 
+	//количество палиндромов mirrorCount 
+	int number = 1, naturalNum = 0, digit = 0, mirror = 0, mirrorCount = 0;
+
+	cin >> K;
+
+	while (number <= K)
+	{
+		//копируем перебираемое число для отзеркаливания
+		naturalNum = number;
+		//делаем зеркальную копию числа
+		while (naturalNum > 0)
+		{
+			digit = naturalNum % 10;
+			mirror = mirror * 10 + digit;
+			naturalNum /= 10;
+		}
+		//проверка на равенство зеркальной копии и оригинала
+		if (number == mirror)
+		{
+			mirrorCount++;
+		}
+		
+		mirror = 0;
+		number++;
+	}
+
+	cout << mirrorCount;
+}
